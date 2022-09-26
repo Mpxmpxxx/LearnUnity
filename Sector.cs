f91fb0fcd45c8bb98483102e0906fcf2d752ca0d
Mpxmpxxx <114412882+Mpxmpxxx@users.noreply.github.com>
2022-09-26 18:39:13 +0300

    Add files via upload

---
+++ Sector.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sector : MonoBehaviour
+{
+   private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.TryGetComponent(out Player player))
+        {
+            player.Bounce();
+        }
+    }
+}//
